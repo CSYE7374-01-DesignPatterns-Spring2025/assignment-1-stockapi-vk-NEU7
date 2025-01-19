1954a044158556126d58b3dc832e0ae3a114e445
Vinay Kumar Chelpuri <chelpuri.v@northeastern.edu>
2025-01-19 17:44:45 -0500

    working solution all cases

--- src/main/java/edu/neu/csye7374/AdobeStock.java
+++ src/main/java/edu/neu/csye7374/AdobeStock.java
@@ -9,10 +9,10 @@ public class AdobeStock extends StockAPI {
     public void setBid(String bid) {
         Double currPrice = this.getPrice();
         Double currBid = Double.parseDouble(bid);
-        setMetric((int) ((currBid - currPrice) * 2));
+        setMetric((int) ((currBid - currPrice) * 2)); // Metric calculation simply deduct the bid from the price and multiply by 2
         if(currBid < currPrice)
-            setPrice(currPrice - currBid * 0.1);
+            setPrice(currPrice - currBid * 0.1); // If bid is less than price, reduce the price by 10% of the bid
         else
-            setPrice(currPrice + currBid * 0.1);
+            setPrice(currPrice + currBid * 0.1); // If bid is more than price, increase the price by 10% of the bid
     }
 }
--- src/main/java/edu/neu/csye7374/AppleStock.java
+++ src/main/java/edu/neu/csye7374/AppleStock.java
@@ -9,11 +9,11 @@ public class AppleStock extends StockAPI {
     public void setBid(String bid) {
         Double currPrice = this.getPrice();
         Double currBid = Double.parseDouble(bid);
-        setMetric((int) ((currBid - currPrice) * 3));
+        setMetric((int) ((currBid - currPrice) * 3)); // Metric calculation simply deduct the bid from the price and multiply by 3
         if(currBid < currPrice)
-            setPrice(currPrice - currBid * 0.2);
+            setPrice(currPrice - currBid * 0.2); // If bid is less than price, reduce the price by 20% of the bid
         else
-            setPrice(currPrice + currBid * 0.2);
+            setPrice(currPrice + currBid * 0.2); // If bid is more than price, increase the price by 20% of the bid
     }
     
 }
